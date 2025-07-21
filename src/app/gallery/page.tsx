67fcd3bb1d7332c7a067f70fc1270b921dee6057
Grigarin-5 <grigarin.baby@bairuhatech.com>
2025-07-21 11:00:10 +0100

    Gallery redesigned

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -118,7 +118,7 @@ const GalleryPage = () => {
                 images={images}
                 title={title}
                 color="tricolor"
-                headingClassName="font-bold text-center pt-5"
+                headingClassName="font-bold"
               />
             </section>
           ))
