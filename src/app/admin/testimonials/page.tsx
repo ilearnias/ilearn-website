1fd8105d1782f809385849269a94c23b7d1453cc
Asfaq0102 <asfaqhussain29@gmail.com>
2025-07-26 13:01:57 +0530

    updated

--- src/app/admin/testimonials/page.tsx
+++ src/app/admin/testimonials/page.tsx
@@ -185,13 +185,19 @@ const Testimonials = () => {
             setMediaList((prev) =>
               prev.filter((item) => item.id !== record.id)
             );
-            setTotalItems((prev) => prev - 1);
+            setMeta((prev) => ({
+              ...prev,
+              itemCount: prev.itemCount - 1,
+            }));
 
             // If this was the last item on the current page, go to previous page
-            const isLastItemOnPage = mediaList.length === 1 && currentPage > 1;
+            const isLastItemOnPage = mediaList.length === 1 && meta.page > 1;
             if (isLastItemOnPage) {
-              setCurrentPage((prev) => prev - 1);
-              // fetchMedia will be triggered by useEffect when currentPage changes
+              setMeta((prev) => ({
+                ...prev,
+                page: prev.page - 1,
+              }));
+              // fetchMedia will be triggered by useEffect when meta.page changes
             }
 
             message.success("Testimonial deleted successfully");
@@ -273,13 +279,6 @@ const Testimonials = () => {
     setEditingMedia(null);
     form.resetFields();
     setThumbnailFile([]);
-  };
-
-  const handlePageChange = (page: number, size?: number) => {
-    setCurrentPage(page);
-    if (size && size !== pageSize) {
-      setPageSize(size);
-    }
   };
 
   const handleThumbnailChange = ({ fileList }: { fileList: UploadFile[] }) => {
